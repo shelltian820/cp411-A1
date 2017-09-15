917d0bd5e84cdfb3206d0ea7f9c8ed625b0c03af
Shelley Tian <shelley@Shelleys-MacBook-Air.local>
2017-09-15 00:23:35 -0600

    starting

--- modelviewer.cpp
+++ modelviewer.cpp
@@ -1,13 +1,18 @@
-#include <stdio.h>
+
 #include <string.h>
 #include <math.h>
+#include <iostream>
+#include <fstream>
 
 
 
-
-int main(argv, argc){
-
-
+int main(int argc, char *argv[]){
+  if (argc != 2){
+    std::cout << "Invalid. Please input 1 file only.\n";
+    return 1;
+  }
+  std::string file = argv[1];
+  std::cout << "file name:" << file << std::endl;
 
 
   return 0;
