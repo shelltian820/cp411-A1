b8be25667f5387c24ab229b160823d0a40e5ea5c
Shelley Tian <shelley@Shelleys-MacBook-Air.local>
2017-09-17 15:07:41 -0600

    reads file, but doesn't write file yet

--- cpp_examples.cpp
+++ cpp_examples.cpp
@@ -1,40 +1,89 @@
 
-// c++ examples program
+// c++ examples of common problems
 #include <iostream>
 #include <string>
 #include <regex>
-
+#include <stdio.h>
+#include <vector>
 using namespace std;
 
-void regex_num(string str);
-
-int main()
-{
-  string str = "v 0 53 0.11 913.0 -4.234234";
-  regex_num(str);
-  string a = "q";
+void compare_strings(){
+  string a = "q"; //must define a and b as strings first
   string b = "p";
   int val = a.compare(b);
   cout << val << endl;
 }
 
-
-void regex_num(string str){
+// how to use regex
+void regex_num(){
+  string str = "v 99874525 -53 0.111 913.0 -4.123456";
+  cout << str << endl;
   regex re("(-)?\\d+((\\.)?\\d+)?");
   sregex_iterator begin(str.begin(), str.end(), re);
   sregex_iterator end;
-  string *output;
-  int size = 1;
-  output = new string[size];
   //auto begin = std::sregex_iterator(str.begin(), str.end(), re);
   //auto end = std::sregex_iterator();
-  int n = 0;
+  int n =0;
   for (std::sregex_iterator i = begin; i != end; ++i) {
         std::smatch match = *i;
         std::string match_str = match.str();
-        output[n] = match_str;
-        cout << output[n] << endl;
-        n++;
-        size++;
+        cout << match_str << "  ";
   }
+}
+
+//stod
+void strtod ()
+{
+  string orbits ("365.24 29.53 5");
+  std::string::size_type sz;     // alias of size_t
+
+  double earth = std::stod (orbits,&sz);
+  double moon = std::stod (orbits.substr(sz)); //????
+  cout << earth << endl;
+  cout << moon << endl;
+
+  string bob("-12.000300");
+  std::string::size_type sz2;
+  double bobby = std::stod (bob,&sz2);
+  cout << bobby << endl;
+}
+
+//how to use vectors
+void vectors(){
+  vector <int> g1;
+  vector <int> :: iterator i;
+
+  //populating vector
+  for (int i = 1; i <= 5; i++)
+      g1.push_back(i);
+
+  //iterate through vector
+  cout << "Output of begin and end\t:\t";
+  for (i = g1.begin(); i != g1.end(); ++i)
+      cout << *i << '\t';
+  cout << endl;
+
+  //accessing new info
+  cout << "Size : " << g1.size();
+  cout << "\nCapacity : " << g1.capacity();
+  cout << "Reference operator [g] : g1[2] = " << g1[2];
+  cout << endl;
+  cout << "at : g1.at(4) = " << g1.at(4);
+  cout << endl;
+  cout << "front() : g1.front() = " << g1.front();
+  cout << endl;
+  cout << "back() : g1.back() = " << g1.back();
+  cout << endl;
+
+}
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+  //regex_num();
+  //strtod();
+  vectors();
 }
--- modelviewer.cpp
+++ modelviewer.cpp
@@ -5,71 +5,133 @@
 #include <fstream>
 #include <stdlib.h>
 #include <regex>
+#include <vector>
 
 using namespace std;
 
 //function prototypes
-void read_lines(ifstream file);
+void read(string filename, vector<double> &v, vector<int> &f);
+vector<double> split_v(string str);
+vector<int> split_f(string str);
 
 //globals
 string obj_name;
-double *temp;
-int v_count, f_count;
-double v_array[] = malloc(sizeof(double));
-double f_array[] = malloc(sizeof(double));
 
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]){
   if (argc != 2){
     cout << "Invalid. Please input one file.\n";
     exit(1); //terminate with error
   }
-  //cout << "opening file " << argv[1] << endl;
+
+
+  //ask for user command repeatedly
+  string command, q="q",w="w";
+
+  while (command.compare(q)!= 0){
+    cout << "\nCommands: Quit(q)\nEnter command:";
+    cin >> command;
+    cout << "you entered: " << command << endl;
+    if (command.compare(w)==0){
+      vector<double> v_vector;
+      vector<int> f_vector;
+      read(argv[1], v_vector, f_vector);
+      vector <double> :: iterator i;
+      vector <int> :: iterator j;
+      //print result
+      cout << "v_vector: \n";
+      for (i = v_vector.begin(); i != v_vector.end(); ++i)
+        cout << *i << " ";
+      cout << endl;
+      cout << "f_vector: \n";
+      for (j = f_vector.begin(); j != f_vector.end(); ++j)
+        cout << *j << " ";
+      cout << endl;
+    }
+  }
+  //end program
+  cout << "Goodbye!\n";
+  return 0;
+}
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+void read(string filename, vector<double> &v, vector<int> &f){
   ifstream infile;
-  //open file
-  infile.open(argv[1]);
+  infile.open(filename);
   if (!infile) {
         cout << "Unable to open file\n";
         exit(2); //terminate with error
   }else{
-    //read data into arrays & variables
-    read_lines(infile);
-  }
+    string line;
+    regex re("(-)?\\d+((\\.)?\\d+)?");
+    while ( getline (infile,line) ){
+      if (line[0]=='v'){
+        //add to vertex array
+        cout << line << endl;
+        vector<double> vertices;
+        vertices = split_v(line);
+        vector <double> :: iterator i;
+        for (i = vertices.begin(); i != vertices.end(); ++i)
+          v.push_back(*i);
+      }
+      if(line[0]=='f'){
+        //add to face array
+        cout << line << endl;
+        vector<int> faces;
+        faces = split_f(line);
+        vector <int> :: iterator j;
+        for (j = faces.begin(); j != faces.end(); ++j)
+          f.push_back(*j);
 
-  //ask for user command repeatedly
-  string command;
-  while (command != "q"){
-    cin >> command;
-    cout << "command: " << command << endl;
-
+      }
+    }
   }
-  //end program
   infile.close();
-  cout << "Exiting...\n";
-  return 0;
-}
-
-
-void read_lines(ifstream file){
-  string line;
-  regex re("(-)?\\d+((\\.)?\\d+)?");
-  while ( getline (file,line) )
-  {
-    if (line[0]=='v'){
-      //add to vertex array
-      cout << line;
-    }
-    else if(line[0]=='f'){
-      //add to face array
-      cout << line;
-    }
-    else if(line[0]=='o'){
-      //store object name
-      cout << line;
-    }
-
-  }
 }
 void write_to_file(){
   ofstream outfile;
   outfile.open("out.obj");
+}
+
+vector<double> split_v(string str){
+  regex re("(-)?\\d+((\\.)?\\d+)?");
+  sregex_iterator begin(str.begin(), str.end(), re);
+  sregex_iterator end;
+  vector<double> output;
+  int n =0;
+  for (std::sregex_iterator i = begin; i != end; ++i) {
+        std::smatch match = *i;
+        std::string match_str = match.str();
+        //convert string to double
+        std::string::size_type sz;
+        double d = std::stod (match_str,&sz);
+        //add to output vector
+        output.push_back(d);
+        n++;
+  }
+  return output;
+}
+
+vector<int> split_f(string str){
+  regex re("\\d+"); //look for integers
+  sregex_iterator begin(str.begin(), str.end(), re);
+  sregex_iterator end;
+  vector<int> output;
+  int n =0;
+  for (std::sregex_iterator i = begin; i != end; ++i) {
+        std::smatch match = *i;
+        std::string match_str = match.str();
+        //convert string to int
+        std::string::size_type sz;
+        double k = std::stoi (match_str,&sz);
+        //add to output vector
+        output.push_back(k);
+        n++;
+  }
+  return output;
 }
